37487db7d1e9d47007e909c73987cddfb82a5ebf
Shivji Kumar <95332763+Shivji-kumar@users.noreply.github.com>
2023-10-07 02:42:54 +0530

    Add files via upload

---
+++ Enotes_SpringBoot/src/main/java/com/enotes/config/CustomeUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.enotes.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.enotes.entity.User;
+import com.enotes.repository.UserRepository;
+
+@Service
+public class CustomeUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User Not Found");
+
+		} else {
+			return new CustomeUser(user);
+		}
+		
+	}
+
+}
